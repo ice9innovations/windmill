2b65a7dd3bb5299c2723e14a519cf5a04b30e1c4
adunderwood <6363445+adunderwood@users.noreply.github.com>
2026-08-01 01:54:40 -0700

    Updated relay.sh with IP binding information

--- relay.sh
+++ relay.sh
@@ -1,2 +1,7 @@
 source windmill_venv/bin/activate
+
+export IMAGE_RELAY_BIND_HOST=192.168.0.101
+export IMAGE_RELAY_PORT=8787
+export IMAGE_RELAY_ALLOWED_CIDRS=192.168.0.0/16,127.0.0.0/8
+
 python3 image_relay.py
