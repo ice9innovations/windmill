0a2bb3949187db6b1ac34683a6914b9d7915badf
adunderwood <6363445+adunderwood@users.noreply.github.com>
2026-09-23 02:02:14 -0700

    Added worker for nsfw_segmentation

---
+++ workers/nsfw_segmentation_worker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""NSFWSegmentationWorker - NSFW segmentation ML service worker."""
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+from base_worker import BaseWorker
+
+
+class NSFWSegmentationWorker(BaseWorker):
+    """Worker for the NSFW segmentation service."""
+
+    def __init__(self):
+        super().__init__('primary.nsfw_segmentation')
+
+
+if __name__ == "__main__":
+    worker = NSFWSegmentationWorker()
+    worker.start()
